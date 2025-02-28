cb31f58b6775adbd1ddc37087894fc3f831ed8c4
Tomasz Paruzel <tom.krzy.par@gmail.com>
2025-02-28 22:55:14 +0100

    we   web functionality done, styling needed

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+try {
+    const response = await axios.get("https://uselessfacts.jsph.pl/random.json");
+    res.render("index.ejs", { fact: response.data.text });
+
+}catch (error) {
+    res.render("index.ejs", { fact: "Sorry, could not get the fact" });
+}
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
