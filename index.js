430ad67b8e8619ecf8e8e4578a6d0ecf001d383a
Tomasz Paruzel <tom.krzy.par@gmail.com>
2025-03-01 18:46:17 +0100

    a bit of styling done

--- index.js
+++ index.js
@@ -7,16 +7,15 @@ const port = 3000;
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-try {
-    const response = await axios.get("https://uselessfacts.jsph.pl/random.json");
+  try {
+    const response = await axios.get(
+      "https://uselessfacts.jsph.pl/random.json"
+    );
     res.render("index.ejs", { fact: response.data.text });
-
-}catch (error) {
+  } catch (error) {
     res.render("index.ejs", { fact: "Sorry, could not get the fact" });
-}
+  }
 });
-
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
